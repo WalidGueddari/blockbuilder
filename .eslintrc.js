2c92bbb6454f8df5f803147af2f61d9332f05f59
mohamed khalil bchir <101249582+Khalil-Bchir@users.noreply.github.com>
2024-07-06 16:54:32 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:tailwindcss/recommended'
+  ],
+  plugins: ['tailwindcss'],
+  rules: {
+    'tailwindcss/no-custom-classname': 'off'
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  env: {
+    node: true
+  }
+}
